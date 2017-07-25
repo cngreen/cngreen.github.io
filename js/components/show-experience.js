1d5a0d246890a740eb55441a5cedc748eb2822f3
cngreen <cngreen@umich.edu>
2017-07-25 15:13:04 -0500

    Update show-experience.js

--- js/components/show-experience.js
+++ js/components/show-experience.js
@@ -3,12 +3,11 @@ $(document).ready(function() {
     $('a[href = "#show_GE"]').click(function(){
         if (document.getElementById("GE_link").innerHTML == "Learn More"){
             document.getElementById("GE_info").innerHTML = "</br>From May 2017 until July 2017 I worked as a software " +
-            "engineering intern within the Edison program at GE Healthcare as a part of the CT image reconstruction "+
-            "team.</br> My project focused on optimizing the software build system.  The previous build system was taking more than 5 hours to compile their code.  " +
+            "engineering intern at GE Healthcare.</br> My project focused on optimizing the software build system.  The previous build system was taking more than 5 hours to compile their code.  " +
             "In order to improve efficiency, I created a containerized and automated build environment using "+
             "Jenkins and Docker. " +
-            "I was in charge of scripting the pipeline in Jenkins using Groovy and JSON files obtained from source code management.  In "+
-            "addition, I needed to create and prepare the Docker container environment with the various dependencies required "+
+            "I scripted the pipeline in Jenkins using Groovy and JSON files obtained from source code management.  In "+
+            "addition, I created the Docker container environment with all of the dependencies needed "+
             "for source code compilation.  I also prepared Makefiles to create source code rpm packages for easy integration into the new build environment.  "+
             "This new build environment is able to compile code 5x faster than the previous system.</br>";
             document.getElementById("GE_link").innerHTML = "Hide";
